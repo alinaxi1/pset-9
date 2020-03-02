1cda62114fa2a2cee824b0a6905c2d8d84de63e3
alinaxi1 <55397901+alinaxi1@users.noreply.github.com>
2020-03-01 21:12:20 -0500

    oh to be a potato that doesn't have to do compsci

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -81,14 +81,14 @@ function collisionDetection() {
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = "#000000";
   ctx.fill();
   ctx.closePath();
 }
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = "#000000";
   ctx.fill();
   ctx.closePath();
 }
@@ -102,7 +102,7 @@ function drawBricks() {
         bricks[c][r].y = brickY;
         ctx.beginPath();
         ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = "#0095DD";
+        ctx.fillStyle = "#000000";
         ctx.fill();
         ctx.closePath();
       }
@@ -111,12 +111,12 @@ function drawBricks() {
 }
 function drawScore() {
   ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = "#000000";
   ctx.fillText("Score: "+score, 8, 20);
 }
 function drawLives() {
   ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = "#000000";
   ctx.fillText("Lives: "+lives, canvas.width-65, 20);
 }
 
